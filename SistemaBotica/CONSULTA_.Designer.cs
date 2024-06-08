44004ee8ea73e7fb321a9dd622aa85deff5d7438
BurBear <burbear03@gmail.com>
2024-06-08 01:21:29 -0500

    Se creo el formulario Consulta Productos Vendidos por Fecha.

--- SistemaBotica/CONSULTA_.Designer.cs
+++ SistemaBotica/CONSULTA_.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CONSULTA_";
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            dateTimePicker1 = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(295, 80);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(107, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(378, 21);
+            label1.TabIndex = 1;
+            label1.Text = "CONSULTAR PRODUCTOS VENDIDOS POR FECHA\r\n";
+            label1.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Seleccione la fecha :\r\n";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(21, 415);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Regresar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(21, 132);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(537, 250);
+            dataGridView1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(21, 80);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(243, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // CONSULTA_
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(597, 450);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "CONSULTA_";
+            Text = "CONSULTA_";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private DateTimePicker dateTimePicker1;
     }
 }
