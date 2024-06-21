4004e0dfe7ed9190c6562862f76208e2c0d85830
yersonnn <benitesyerson@gmail.com>
2024-06-20 23:05:56 -0500

    .

--- SistemaBotica/CRUD_MetodoPago.Designer.cs
+++ SistemaBotica/CRUD_MetodoPago.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CRUD_MetodoPago";
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(436, 372);
+            button3.Name = "button3";
+            button3.Size = new Size(170, 38);
+            button3.TabIndex = 19;
+            button3.Text = "INHABILITAR ";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(436, 317);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 38);
+            button2.TabIndex = 18;
+            button2.Text = "MODIFICAR DATOS";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(436, 262);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 38);
+            button1.TabIndex = 17;
+            button1.Text = "AGREGAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            groupBox1.Location = new Point(12, 250);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(378, 138);
+            groupBox1.TabIndex = 16;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "DETALLES";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 9.75F);
+            textBox2.Location = new Point(154, 67);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(193, 25);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(154, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(193, 25);
+            textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(63, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(69, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Codigo:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 47);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(607, 181);
+            dataGridView1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(210, 30);
+            label1.TabIndex = 14;
+            label1.Text = "METODOS DE PAGO";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(88, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Tipo:";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 9.75F);
+            textBox3.Location = new Point(154, 100);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(193, 25);
+            textBox3.TabIndex = 11;
+            // 
+            // CRUD_MetodoPago
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(660, 432);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "CRUD_MetodoPago";
+            Text = "CRUD_MetodoPago";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox3;
+        private Label label4;
     }
 }
